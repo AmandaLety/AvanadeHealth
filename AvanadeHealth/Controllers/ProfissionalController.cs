dfadcd9c7a22d1c11c7939df7557483250e42b4a
Amanda Leticia <a.de.siqueira.camelo@avanade.com>
2022-12-20 10:59:21 -0300

    Adicionar arquivos de projeto.

---
+++ AvanadeHealth/Controllers/ProfissionalController.cs
@@ -0,0 +1,107 @@
+﻿using AvanadeHealth.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AvanadeHealth.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProfissionalController : Controller
+    {
+        private readonly ProfissionalRepository profissionalRepository;
+
+        public ProfissionalController()
+        {
+            profissionalRepository = new ProfissionalRepository();
+        }
+
+        [HttpGet]
+        [Route("/ListarTodos")]
+        public IActionResult Consultar()
+        {
+            try
+            {
+                var listaProfissional = profissionalRepository.Listar();
+                return Ok(listaProfissional);
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+        }
+
+        // Anotação de uso do Verb HTTP Get
+        [HttpGet]
+        [Route("/ConsultarPorId/{id}")]
+
+        public ActionResult Consultar(int id)
+        {
+            try
+            {
+                var tipoProfissional = profissionalRepository.Consultar(id);
+                if (tipoProfissional.IdProfissional != 0)
+                {
+                    return Ok(tipoProfissional);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+        }
+
+
+        [HttpPost]
+        [Route("/CadastrarProfissional")]
+        public ActionResult Cadastrar(Entidade.Profissional profissional)
+        {
+            try
+            {
+                profissionalRepository.Inserir(profissional);
+                return Ok(profissional);
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+
+        }
+
+
+        [HttpPut]
+        [Route("/AtualizarProfissional")]
+        public ActionResult Editar(Entidade.Profissional IdProfissional)
+        {
+
+            try
+            {
+                profissionalRepository.Alterar(IdProfissional);
+                return Ok();
+            }
+            catch (KeyNotFoundException)
+            {
+                return BadRequest();
+            }
+        }
+
+
+
+        [HttpDelete]
+        [Route("/Excluir")]
+        public ActionResult Excluir(int Id)
+        {
+            try
+            {
+                profissionalRepository.Excluir(Id);
+                return Ok();
+            }
+            catch (KeyNotFoundException)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ AvanadeHealth/Repository/ProfissionalRepository.cs
@@ -0,0 +1,176 @@
+﻿using AvanadeHealth.Entidade;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AvanadeHealth.Repository
+{
+    public class ProfissionalRepository
+    {
+        public IList<Profissional> Listar()
+
+        {
+            IList<Profissional> lista = new List<Profissional>();
+            var connectionString = new ConfigurationBuilder()
+                                            .SetBasePath(Directory.GetCurrentDirectory())
+                                            .AddJsonFile("appsettings.json")
+                                            .Build().GetConnectionString("SqlHealth");
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                String query =
+                    "SELECT IDPROFISSIONAL, NOME, TELEFONE, ENDERECO, ATIVO FROM PROFISSIONAL  ";
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+                SqlDataReader dataReader = command.ExecuteReader();
+
+
+                while (dataReader.Read())
+                {
+                    // Recupera os dados
+                    Profissional profissional = new Profissional();
+                    profissional.IdProfissional = Convert.ToInt32(dataReader["IDPROFISSIONAL"]);
+                    profissional.Nome = dataReader["NOME"].ToString();
+                    profissional.Telefone = dataReader["TELEFONE"].ToString();
+                    profissional.Endereco = dataReader["ENDERECO"].ToString();
+                    profissional.Ativo = dataReader["ATIVO"].Equals("1");
+
+
+                    // Adiciona o modelo da lista
+                    lista.Add(profissional);
+                }
+                connection.Close();
+            } // Finaliza o objeto connection
+            // Retorna a lista
+            return lista;
+        }
+        public Profissional Consultar(int id)
+        {
+            Profissional profissional = new Profissional();
+
+            //a conexão não muda
+            var connectionString = new ConfigurationBuilder()
+                                        .SetBasePath(Directory.GetCurrentDirectory())
+                                        .AddJsonFile("appsettings.json")
+                                        .Build().GetConnectionString("SqlHealth");
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                String query =
+                    "SELECT IDPROFISSIONAL, NOME, TELEFONE, ENDERECO, ATIVO FROM PROFISSIONAL WHERE IDPROFISSIONAL = @IDPROFISSIONAL ";
+
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.Add("@IDPROFISSIONAL", SqlDbType.Int);
+                command.Parameters["@IDPROFISSIONAL"].Value = id;
+                connection.Open();//ABRIR CONEXÃO
+
+
+                SqlDataReader dataReader = command.ExecuteReader();//EXECUTA COMANDO
+
+
+                while (dataReader.Read())
+                {
+                    // Recupera os dados
+
+                    profissional.IdProfissional = Convert.ToInt32(dataReader["IDPROFISSIONAL"]);
+                    profissional.Nome = dataReader["NOME"].ToString();
+                    profissional.Telefone = dataReader["TELEFONE"].ToString();
+                    profissional.Endereco = dataReader["ENDERECO"].ToString();
+                    profissional.Ativo = dataReader["ATIVO"].Equals("1");
+
+                }
+                connection.Close();
+            } // Finaliza o objeto connection
+
+            // Retorna a lista para view
+            return profissional;
+        }
+        public void Inserir(Profissional profissional)
+        {
+            var connectionString = new ConfigurationBuilder()
+                                        .SetBasePath(Directory.GetCurrentDirectory())
+                                        .AddJsonFile("appsettings.json")
+                                        .Build().GetConnectionString("SqlHealth");
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                String query =
+                    "INSERT INTO PROFISSIONAL ( NOME, TELEFONE, ENDERECO, ATIVO ) VALUES ( @nome, @telefone, @endereco, @ativo ) ";
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                // Adicionando o valor ao comando
+                command.Parameters.Add("@nome", SqlDbType.Text);
+                command.Parameters.Add("@telefone", SqlDbType.Text);
+                command.Parameters.Add("@endereco", SqlDbType.Text);
+                command.Parameters.Add("@ativo", SqlDbType.Int);
+                command.Parameters["@nome"].Value = profissional.Nome;
+                command.Parameters["@telefone"].Value = profissional.Telefone;
+                command.Parameters["@endereco"].Value = profissional.Endereco;
+                command.Parameters["@ativo"].Value = profissional.Ativo.Equals("1");
+
+
+                // Abrindo a conexão com  o Banco
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+        public void Alterar(Profissional profissional)
+        {
+            var connectionString = new ConfigurationBuilder()
+                                        .SetBasePath(Directory.GetCurrentDirectory())
+                                        .AddJsonFile("appsettings.json")
+                                        .Build().GetConnectionString("SqlHealth");
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                String query =
+                    "UPDATE PROFISSIONAL SET NOME = @nome, TELEFONE = @telefone, ENDERECO = @endereco, ATIVO = @ativo WHERE IDPROFISSIONAL = @IdProfissional  ";
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                // Adicionando o valor ao comando
+
+                command.Parameters.Add("@idprofissional", SqlDbType.Int);
+                command.Parameters.Add("@nome", SqlDbType.Text);
+                command.Parameters.Add("@telefone", SqlDbType.Text);
+                command.Parameters.Add("@endereco", SqlDbType.Text);
+                command.Parameters.Add("@ativo", SqlDbType.Int);
+                command.Parameters["@idprofissional"].Value = profissional.IdProfissional;
+                command.Parameters["@nome"].Value = profissional.Nome;
+                command.Parameters["@telefone"].Value = profissional.Telefone;
+                command.Parameters["@endereco"].Value = profissional.Endereco;
+                command.Parameters["@ativo"].Value = profissional.Ativo.Equals("1");
+
+
+                // Abrindo a conexão com  o Banco
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+        public void Excluir(int id)
+        {
+            var connectionString = new ConfigurationBuilder()
+                                        .SetBasePath(Directory.GetCurrentDirectory())
+                                        .AddJsonFile("appsettings.json")
+                                        .Build().GetConnectionString("SqlHealth");
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                String query =
+                    "DELETE PROFISSIONAL WHERE IDPROFISSIONAL = @idprofissional  ";
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                // Adicionando o valor ao comando
+                command.Parameters.Add("@idprofissional", SqlDbType.Int);
+                command.Parameters["@idprofissional"].Value = id;
+
+                // Abrindo a conexão com  o Banco
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+    }
+}
